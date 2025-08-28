c5cdac2ea6104dcaf231a34fe152165738e13917
Vadim Yemtsov <hardmagnett@yandex.ru>
2025-08-28 18:34:48 +0300

    Meta

--- src/10_app/App.vue
+++ src/10_app/App.vue
@@ -3,7 +3,9 @@
 <template>
   <div>
     <router-link :to="{name: 'main'}">Главная</router-link>
+    &nbsp;&nbsp;&nbsp;
     <router-link :to="{name: 'projects'}">Проекты</router-link>
+    &nbsp;&nbsp;&nbsp;
     <router-link :to="{name: 'projects'}">Обо мне</router-link>
   </div>
   <router-view />
--- src/20_pages/ProjectPage/ui/ProjectPage.vue
+++ src/20_pages/ProjectPage/ui/ProjectPage.vue
@@ -5,6 +5,13 @@
     <p v-if="project.projectPhaseQty">{{project.projectPhaseQty.title}}</p>
     <img width="100" :src="project.imageMain">
 
+    <router-link :to="{
+        name: 'project',
+        params: { projectSlug: 'project-2' }
+      }">
+      <p>project-2</p>
+    </router-link>
+
   </div>
 </template>
 
@@ -20,22 +27,21 @@ const projectsApi = new ProjectsApi()
 let project = ref<Project|null>(null)
 
 
-onBeforeMount(()=>{
-  console.log('beforeMount')
-  let slug = route.params.projectSlug as string ?? '' as string
+const getProject = (slug: string)=>{
   const projectThatFound = projectsApi.getProjectBySlug({slug: slug})
-  // console.log(projectThatFound); console.log('^...projectThatFound:')
   if (!projectThatFound) {
-    router.push({name: 'NotFound', replace: false })
+    router.push({name: 'NotFound'})
   }
   if (projectThatFound) {
     project.value = projectThatFound
     setMetaTags(project.value.meta)
   }
-})
+}
+
+getProject(route.params.projectSlug as string)
+
 onBeforeRouteUpdate(async (to, from)=>{
-  console.log(to); console.log('^...to:')
-  console.log('in comp')
+  getProject(to.params.projectSlug as string)
 })
 
 </script>
